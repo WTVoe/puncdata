66a7beba14ecf568dd6188e9edbacfae8d07fd2c
WTVoe <wt.voellinger@hotmail.com>
2026-03-20 11:12:13 +0100

    fixed a mixup of id and index during file import

--- scripts/script_file.js
+++ scripts/script_file.js
@@ -224,7 +224,7 @@ class FileList{
     import(save){
         for(let i=0; i<save.list.length; i++){
             const saveFile = save.list[i]
-            let file = new File(saveFile.name, saveFile.index, saveFile.id, this)
+            let file = new File(saveFile.name, saveFile.id, saveFile.index, this)
             file.import(saveFile)
             this.list.push(file)
         }
